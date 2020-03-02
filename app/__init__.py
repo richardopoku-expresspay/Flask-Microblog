d9ec5a9aa2c6bea5b65861d043f72390bfa52fb4
Richard Opoku <richardopoku@expresspaygh.com>
2020-03-02 08:33:21 +0000

    created the config for the various environments under which the app can be run.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config.instance import app_config
 
 app = Flask(__name__)
+app.config.from_object(app_config)
+app.config.from_pyfile('../config/instance.py')
 
 from app import routes
