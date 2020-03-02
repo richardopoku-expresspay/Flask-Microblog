fb214411e899b645166839078d7f395a3411a66f
Richard Opoku <richardopoku@expresspaygh.com>
2020-03-02 09:46:34 +0000

    using named routes now instead of manually typing the endpoints.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -13,5 +13,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login request for {}'.format(form.username.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
