0b7e17f1647a245c33cda37e19f47b56e281cb82
Richard Opoku <richardopoku@expresspaygh.com>
2020-03-02 11:21:26 +0000

    inititated processes to start user authentication

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
 
+@app.shell_context_processor
+def make_shell_context():
+    from app import db
+    from app.models import User, Post
+    return {'db': db, 'User': User, 'Post': Post}
+
 if __name__=='__main__':
     app.run()
